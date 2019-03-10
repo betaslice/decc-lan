b0983ee1f5878950e687201a0aca52f4f8d276a5
techytom11 <dinguslemingus@gmail.com>
2019-03-10 14:43:55 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,25 +1,54 @@
 # Work with Python 3.6
-import discord
+import random
+import asyncio
+import aiohttp
+import json
+from discord import Game
+from discord.ext.commands import Bot
 
-TOKEN = 'NTU0Mzc2NDMxOTA4NTUyNzI0.D2bvoQ.YMA9T4jolR_h3kYvzhc3n_gWjYw'
+#Bot Setup
+BOT_PREFIX = ("?", "!")
+TOKEN = "NTU0Mzc2NDMxOTA4NTUyNzI0.D2b5RA.h6WxaqKJKVLylaiKj0C7QJdRw0c"
 
-client = discord.Client()
+client = Bot(command_prefix=BOT_PREFIX)
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+#Commands
+@client.command(name='8ball',
+                description="Answers your pathetic questions.",
+                brief="Answers a question.",
+                aliases=['eight_ball', 'eightball', '8-ball'],
+                pass_context=True)
+async def eight_ball(context):
+    possible_responses = [
+        'Absolutely not.',
+        'My answer is NO.',
+        'Why should I know?',
+        'Possibly.',
+        'The answer finna be yes.',
+    ]
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+
+@client.command()
+async def square(number):
+    squared_value = int(number) * int(number)
+    await client.say(str(number) + " squared is " + str(squared_value))
+
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    await client.change_presence(game=Game(name="with pathetic carbon-based life forms"))
+    print("Logged in as " + client.user.name)
 
+
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers:")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
+
+#Bot Execution
+client.loop.create_task(list_servers())
 client.run(TOKEN)
