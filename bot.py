8f208a45c0d2af883910e766505b3634f95f5936
betaslice <dinguslemingus@gmail.com>
2019-04-17 12:29:42 -0500

    Fixed the bot finally

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ import json
 from discord import Game
 from discord.ext.commands import Bot
 import os
-import datetime
 
 #Bot Setup
 BOT_PREFIX = ("?", "!")
@@ -14,14 +13,13 @@ TOKEN = os.environ['TOKEN']
 
 client = Bot(command_prefix=BOT_PREFIX)
 
-#Commands
 @client.command(name='8ball',
                 description="Answers your pathetic questions.",
                 brief="Answers a question.",
                 aliases=['eight_ball', 'eightball', '8-ball'],
                 pass_context=True)
 async def eight_ball(context):
-    possible_responses = [
+    possible_responses = [   
         'Absolutely not.',
         'My answer is NO.',
         'Why should I know?',
@@ -46,59 +44,25 @@ async def rick_roll(context):
 async def proto_rap(context):
     await client.say("https://www.youtube.com/watch?v=PIp06sdFHLM")
 
-@client.command(name="countryroads",
-                description="take me home",
-                brief="take me home",
-                aliases=[],
+@client.command(name='nsfw',
+                description="Adds NSFW role to person",
+                brief="Add NSFW role",
+                aliases=["addnsfw"],
                 pass_context=True)
-async def country_roads(context):
-                await client.say('https://www.youtube.com/watch?v=IObnt8UFjVA')
 
-@client.command(name="vaporwave",
-                description="",
-                brief="",
-                aliases=[],
-                pass_context=True)
-async def vapor_wave(context):
-    await client.say('https://www.youtube.com/watch?v=jjiq8VogmIk')
-
-@client.command(name="time",
-                description="",
-                brief="say the time",
-                aliases=[],
-                pass_context=true)
-async def tell_time(context):
-    myTime = datetime.now()
-    hour = myTime.hour
-    minute = myTime.minute
-    if not (context=='24'):
-        ampm = 'AM'
-        if(hour > 11):
-            ampm = 'PM'
-            if(hour > 12):
-                hour -= 12
-         await client.say(str(hour) + ':' + str(minute) + ' ' + ampm)
-    else:
-        await client.say(str(hour) + ':' + str(minute)        
+@client.command()
+async def square(number):
+    squared_value = int(number) * int(number)
+    await client.say(str(number) + " squared is " + str(squared_value))
 
 #Game Name
-gamelist = ["3D Space Pinball", "Minesweeper", "Windows 98", "with pathetic life forms"]
+gamelist = ["3D Space Pinball", "Minesweeper", "Windows 98", "with pathetic life forms", "Weeb Circle Game"]
 
 @client.event
-async def on_message(message):
-    await client.process_commands(message)
-    if message.author == client.user:
-        return
-    if 'r/' in message.content:
-        newmessage = message.content.split()
-        for word in newmessage:
-                if 'r/' in word:
-                    msg = ('https://reddit.com/' + word).format(message)
-                    await client.send_message(message.channel, msg)
-                    
 async def on_ready():
     await client.change_presence(game=Game(name=random.choice(gamelist)))
     print("Logged in as " + client.user.name)
+
 
 async def list_servers():
     await client.wait_until_ready()
@@ -107,6 +71,7 @@ async def list_servers():
         for server in client.servers:
             print(server.name)
         await asyncio.sleep(600)
+
 
 #Bot Execution
 client.loop.create_task(list_servers())
